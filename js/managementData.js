f8e69c2d843ef198519a0a448c21ad94ad06b72a
Lauty128 <lautarosilverii@gmail.com>
2022-09-01 08:07:40 -0300

    v2

--- js/managementData.js
+++ js/managementData.js
@@ -1,13 +1,66 @@
 export { managementData }
 
 class managementData {
-    
-    #validateData = ()=>{
-        console.log("HOLANDA PRIVADO")
+
+    deleteData = storage =>{
+        //Clean Local Storage to remove unnecessary data and so avoid errors
+        storage.clear()
     }
 
-    useValidateData = ()=>{
-        this.#validateData()
+    #validateContent = ()=>{             
+        // Checks that the div content hasn't been modified
+                                
     }
+
+    #useValidateData = ()=>{
+        
+    }
+
+    deleteTasks_onScreen = ()=>{
+        // Clean the screen to correctly execute the function "printData"
+    }
+
+    printData = ()=>{
+        // Print the data stored in the local Storage
+    }
+
+    open_modalWindow =()=>{
+        document.querySelector(".modalWindow").classList.add("modalWindow_open")
+        setTimeout(()=>{ document.querySelector(".form_modalWindow").classList.add("form_modalWindow_open") }, 5)
+    }
+
+    close_modalWindow = ()=>{
+        document.querySelector(".form_modalWindow").classList.remove("form_modalWindow_open")
+        setTimeout(()=>{
+            document.querySelector(".modalWindow").classList.remove("modalWindow_open") 
+            document.getElementById("title_input").value = ""
+            document.getElementById("description_input").value = ""
+        }, 300)
+
+    }
+
+    open_modalWindow_toModify = icon =>{
+        //Clean the <inputs>
+        let task = icon.parentNode.parentNode;
+        let tittleTask = task.firstElementChild.textContent
+        let descriptionTask = task.children[1].textContent
+        document.getElementById("title_input").value = tittleTask
+        document.getElementById("description_input").value = descriptionTask
+        this.open_modalWindow()
+    }
+
+    addTask = id=>{
+
+    }
+
+    deleteTask = id=>{
+
+    }
+
+    modifyTask = id=>{
+
+    }
+
+    
 
 }
--- js/script.js
+++ js/script.js
@@ -2,12 +2,23 @@ import { managementData } from "./managementData.js"
 
 let manage = new managementData()   // Gestor de LocalStorage 
 
-manage.useValidateData()
-
 let storage = localStorage
-storage.setItem("persona",[])
-storage.setItem("persona",{nombre:"JUAN", apellido:"Silverii"})
-storage.setItem("persona",{nombre:"JUAN", apellido:"Silverii"})
+
+manage.deleteData(storage)
 
 // APRENDIENDO A USAR LOCALSTORAGE
+
+
+
+document.querySelector(".modalWindow").addEventListener("click",e=>{
+    if(e.target.classList.contains("modalWindow_open") || e.target.getAttribute("id") == "exit_modalWindow"){ 
+        manage.close_modalWindow() 
+    }
+})
+
+document.querySelector(".tasks").addEventListener("click", e=>{
+    if(e.target.getAttribute("id") == "addTask")   manage.open_modalWindow()  //The form opens if you click on "add Task"
+
+    if(e.target.classList.contains("taskEditButton")) manage.open_modalWindow_toModify(e.target) //The form opens if you click on "modification icon"
+})
 
